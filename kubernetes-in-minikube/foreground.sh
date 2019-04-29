490c976ebf8837798d2ccf8a96fc12ba8610e418
Bob Reselman <reselbob@gmail.com>
2019-04-29 12:57:50 -0700

    Correcting typos

--- kubernetes-in-minikube/foreground.sh
+++ kubernetes-in-minikube/foreground.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 uname -a
 
-echo "Cloning courseware from GitHub into environment\r\n"
+echo "Cloning courseware from GitHub into environment"
 
 git clone https://github.com/reselbob/k8s-4-it-pros.git
